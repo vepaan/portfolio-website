167dce8ff1b97a7d91b2ab86b53afbbeb119eaee
vepaan <141347566+vepaan@users.noreply.github.com>
2025-05-02 21:41:00 -0500

    more boilerplate

---
+++ src/app/components/homepage.tsx
@@ -0,0 +1,11 @@
+'use client'
+
+import React, {useState} from 'react';
+
+export default function HomePage() {
+    const [world, setWorld] = useState<string>();
+
+    return (
+        <div>Hello World</div>
+    )
+}
